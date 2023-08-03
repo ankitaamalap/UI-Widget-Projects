9e6eb1b2e1af3bb8f9a8171e88de3babf2d44c33
ankitaamalap <ankitamalap94@gmail.com>
2023-08-03 23:44:53 +0530

    colorText

---
+++ ColorSeekBarLibrary/app/src/main/java/com/example/colorseekbarlibrary/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.colorseekbarlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.rtugeek.android.colorseekbar.ColorSeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView);
+        ColorSeekBar colorSeekBar = findViewById(R.id.colorSeekBar);
+
+        colorSeekBar.setOnColorChangeListener(new ColorSeekBar.OnColorChangeListener() {
+            @Override
+            public void onColorChangeListener(int colorBarPosition, int alphaBarPosition, int color) {
+                textView.setTextColor(color);
+            }
+        });
+
+    }
+}
